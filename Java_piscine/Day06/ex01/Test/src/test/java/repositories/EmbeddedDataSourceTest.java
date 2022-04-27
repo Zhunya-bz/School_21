899e7d43b7f5c2123682747d74500d1ccde4c882
Zhunya-bz <fluffylion2013@gmail.com>
2022-04-27 22:56:01 +0300

    Day06

---
+++ Java_piscine/Day06/ex01/Test/src/test/java/repositories/EmbeddedDataSourceTest.java
@@ -0,0 +1,26 @@
+package repositories;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import java.sql.SQLException;
+
+public class EmbeddedDataSourceTest {
+    private DataSource dataSource;
+
+    @BeforeEach
+    void init() {
+        dataSource = new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.HSQL)
+                .addScript("schema.sql")
+                .addScript("data.sql")
+                .build();
+    }
+
+    @Test
+    void checkGetConnection() throws SQLException {
+        Assertions.assertNotNull(dataSource.getConnection());
+    }
+}
