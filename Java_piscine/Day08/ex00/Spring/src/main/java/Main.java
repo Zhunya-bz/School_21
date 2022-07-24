60687b5e56b7dc26c08384c429f7c59c9638d2e7
Shepherd Altmer <saltmer@un-b6.kzn.21-school.ru>
2022-07-24 14:42:07 +0300

    ex00 with spring bean

--- Java_piscine/Day08/ex00/Spring/src/main/java/Main.java
+++ Java_piscine/Day08/ex00/Spring/src/main/java/Main.java
@@ -1,9 +1,17 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        PreProcessor preProcessor = new PreProcessorToUpperImpl();
-        Renderer renderer = new RendererErrImpl(preProcessor);
-        PrinterWithDateTimeImpl printer = new PrinterWithDateTimeImpl(renderer);
-//        printer.setPrefix("HI");
-        printer.print("Zhunya");
+//        PreProcessor preProcessor = new PreProcessorToUpperImpl();
+//        Renderer renderer = new RendererErrImpl(preProcessor);
+//        PrinterWithDateTimeImpl printer = new PrinterWithDateTimeImpl(renderer);
+////        printer.setPrefix("HI");
+//        printer.print("Zhunya");
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+        Printer printer = context.getBean("prefixErrUpper", Printer.class);
+        printer.print("Hello");
+
     }
 }
