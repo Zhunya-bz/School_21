fbf0bad669455751385e7580a47c6f76eb57c2e3
ekanaeva <kanaeva.em@gmail.com>
2025-04-23 21:10:18 +0300

    c-00

---
+++ C_Piscine_Paris/c-00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ekanaeva <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/15 13:35:09 by ekanaeva          #+#    #+#             */
+/*   Updated: 2025/01/15 13:35:14 by ekanaeva         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	letter;
+
+	letter = 'z';
+	while (letter >= 'a')
+	{
+		write(1, &letter, 1);
+		letter--;
+	}
+}
---
+++ C_Piscine_Paris/c-00/ex05/ft_print_comb.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ekanaeva <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/15 14:47:33 by ekanaeva          #+#    #+#             */
+/*   Updated: 2025/01/15 14:47:34 by ekanaeva         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print(char i, char j, char k)
+{
+	write(1, &i, 1);
+	write(1, &j, 1);
+	write(1, &k, 1);
+	if (i == '7' && j == '8' && k == '9')
+	{
+		write(1, "\n", 1);
+	}
+	else
+	{
+		write(1, ", ", 2);
+	}
+}
+
+void	ft_print_comb(void)
+{
+	char	i;
+	char	j;
+	char	k;
+
+	i = '0';
+	while (i <= '7')
+	{
+		j = i + 1;
+		while (j <= '8')
+		{
+			k = j + 1;
+			while (k <= '9')
+			{
+				ft_print(i, j, k);
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+/*
+int main() {
+	ft_print_comb();
+	return 0;
+}*/
